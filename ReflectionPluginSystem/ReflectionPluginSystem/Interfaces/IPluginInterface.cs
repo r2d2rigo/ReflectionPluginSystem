6cb96f6370bc46206f8b7e109136980727984334
Rodrigo <r2d2rigo@gmail.com>
2014-01-14 16:28:35 +0100

    Added plugin interface definition.

---
+++ ReflectionPluginSystem/ReflectionPluginSystem/Interfaces/IPluginInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ReflectionPluginSystem.Interfaces
+{
+    public interface IPluginInterface
+    {
+        string Name { get; }
+
+        string Description { get; }
+
+        Version Version { get; }
+
+        string GetMessage();
+    }
+}
