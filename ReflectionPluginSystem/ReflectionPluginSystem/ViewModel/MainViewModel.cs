0d89877fe89dbc0e389d716f2e4968352119869f
Rodrigo <r2d2rigo@gmail.com>
2014-01-14 16:42:03 +0100

    Added MVVMLight library.

---
+++ ReflectionPluginSystem/ReflectionPluginSystem/ViewModel/MainViewModel.cs
@@ -0,0 +1,52 @@
+using GalaSoft.MvvmLight;
+using ReflectionPluginSystem.Interfaces;
+using System.Collections.ObjectModel;
+
+namespace ReflectionPluginSystem.ViewModel
+{
+    /// <summary>
+    /// This class contains properties that the main View can data bind to.
+    /// <para>
+    /// Use the <strong>mvvminpc</strong> snippet to add bindable properties to this ViewModel.
+    /// </para>
+    /// <para>
+    /// You can also use Blend to data bind with the tool's support.
+    /// </para>
+    /// <para>
+    /// See http://www.galasoft.ch/mvvm
+    /// </para>
+    /// </summary>
+    public class MainViewModel : ViewModelBase
+    {
+        private const string PluginsFolder = "Plugins";
+
+        private ObservableCollection<IPluginInterface> plugins;
+
+        public ObservableCollection<IPluginInterface> Plugins
+        {
+            get
+            {
+                return this.plugins;
+            }
+            private set
+            {
+                this.plugins = value;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the MainViewModel class.
+        /// </summary>
+        public MainViewModel()
+        {
+            ////if (IsInDesignMode)
+            ////{
+            ////    // Code runs in Blend --> create design time data.
+            ////}
+            ////else
+            ////{
+            ////    // Code runs "for real"
+            ////}
+        }
+    }
+}
