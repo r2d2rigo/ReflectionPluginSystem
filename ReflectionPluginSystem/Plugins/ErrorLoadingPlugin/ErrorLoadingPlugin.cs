46f8c462de1caf83bc0e7a05bc9bf899476cd2d9
Rodrigo Díaz <r2d2rigo@gmail.com>
2014-01-22 01:42:33 +0100

    Added sample plugin projects.

---
+++ ReflectionPluginSystem/Plugins/ErrorLoadingPlugin/ErrorLoadingPlugin.cs
@@ -0,0 +1,32 @@
+﻿using ReflectionPluginSystem.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorLoadingPlugin
+{
+    public class ErrorLoadingPlugin : IPluginInterface
+    {
+        public string Name
+        {
+            get { return "Invalid plugin"; }
+        }
+
+        public string Description
+        {
+            get { return "This plugin will not load"; }
+        }
+
+        public Version Version
+        {
+            get { return new Version(1, 0); }
+        }
+
+        public string GetMessage()
+        {
+            return "This string will not display.";
+        }
+    }
+}
---
+++ ReflectionPluginSystem/Plugins/HelloWorldPlugin/HelloWorldPlugin.cs
@@ -0,0 +1,32 @@
+﻿using ReflectionPluginSystem.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldPlugin
+{
+    public class HelloWorldPlugin : IPluginInterface
+    {
+        public string Name
+        {
+            get { return "Hello World plugin"; }
+        }
+
+        public string Description
+        {
+            get { return "A plugin that shows the \"Hello World!\" message"; }
+        }
+
+        public Version Version
+        {
+            get { return new Version(1, 5); }
+        }
+
+        public string GetMessage()
+        {
+            return "Hello, World!";
+        }
+    }
+}
